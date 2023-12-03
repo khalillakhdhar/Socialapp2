a737439608baa67921701d63def325a81c362bb5
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2023-12-03 13:30:14 +0100

    Initial commit

---
+++ src/main/java/com/webservice/Securityappdemo1Application.java
@@ -0,0 +1,13 @@
+package com.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Securityappdemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Securityappdemo1Application.class, args);
+	}
+
+}
